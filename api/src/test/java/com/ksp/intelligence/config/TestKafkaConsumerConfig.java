43704955bf5973f390f186daee9b74d4a3991af1
archis <gowork.archis@gmail.com>
2026-07-26 23:33:04 +0530

    Fix TestKafkaConsumerConfig generic types

--- api/src/test/java/com/ksp/intelligence/config/TestKafkaConsumerConfig.java
+++ api/src/test/java/com/ksp/intelligence/config/TestKafkaConsumerConfig.java
@@ -11,20 +11,21 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.DefaultErrorHandler;
+import com.ksp.intelligence.model.FirEventDto;
 
 @Configuration
 public class TestKafkaConsumerConfig {
 
     @Bean(name = "firKafkaListenerContainerFactory")
-    public ConcurrentKafkaListenerContainerFactory<String, Object> firKafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    public ConcurrentKafkaListenerContainerFactory<String, com.ksp.intelligence.model.FirEventDto> firKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, FirEventDto> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(dummyConsumerFactory());
         factory.setCommonErrorHandler(new DefaultErrorHandler());
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
         return factory;
     }
 
-    private ConsumerFactory<String, Object> dummyConsumerFactory() {
+    private ConsumerFactory<String, FirEventDto> dummyConsumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put("bootstrap.servers", "dummy:9092");
         props.put("key.deserializer", StringDeserializer.class);
